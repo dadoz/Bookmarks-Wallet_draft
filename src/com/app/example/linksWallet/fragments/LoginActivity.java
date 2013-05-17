ee3ef13899b278a26f5486f2743e550f716ad73e
Davide Lamanna <davide.lamanna@quid.it>
2013-05-17 10:52:54 +0200

    first commit - upload last version of app

---
+++ src/com/app/example/linksWallet/fragments/LoginActivity.java
@@ -0,0 +1,60 @@
+package com.app.example.linksWallet.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+import com.app.example.linksWallet.BaseActivity;
+import com.app.example.linksWallet.R;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+
+public class LoginActivity extends BaseActivity {
+	
+	private Fragment mContent;
+	
+	public LoginActivity() {
+		super(R.string.changing_fragments);
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// set the Above View
+		if (savedInstanceState != null)
+			mContent = getSupportFragmentManager().getFragment(savedInstanceState, "mContent");
+		if (mContent == null)
+			mContent = new LoginFragment();	
+		
+		// set the Above View
+		setContentView(R.layout.content_frame);
+		getSupportFragmentManager()
+		.beginTransaction()
+		.replace(R.id.content_frame,mContent)
+		.commit();
+		
+		// set the Behind View
+		setBehindContentView(R.layout.menu_frame);
+		getSupportFragmentManager()
+		.beginTransaction()
+		.replace(R.id.menu_frame, new ListMenuFragment())
+		.commit();
+		
+		// customize the SlidingMenu
+		getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+	}
+	
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		getSupportFragmentManager().putFragment(outState, "mContent", mContent);
+	}
+	
+	public void switchContent(Fragment fragment) {
+		mContent = fragment;
+		getSupportFragmentManager()
+		.beginTransaction()
+		.replace(R.id.content_frame, fragment)
+		.commit();
+		getSlidingMenu().showContent();
+	}
+
+}
